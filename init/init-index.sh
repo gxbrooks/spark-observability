e85d3ee4b036d0d9e79e99a63741a56315914b90
Gary Brooks <gxbrooks@gmail.com>
2024-06-30 16:15:57 -0500

    Spark logs all successfully processed

--- init/init-index.sh
+++ init/init-index.sh
@@ -5,15 +5,18 @@
 # good format 
 # curl --no-progress-meter -X PUT "https://es01:9200/_index_template/spark-template" --cacert config/certs/ca/ca.crt -u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" -d "@init/cannot-merge-a-non-object-mapping-with-an-object-mapping.json"
 
-<<Comment
+# In spark the depth of the query plans can exceed the index default limit of 20 levels. 
+# The sparkIndexTemplate.json file assumes the namespace of the index and the dataset are both
+# named 'spark'. These names are specified in the logstash.conf file. 
+#
 echo "Creating Index Template"
 curl --no-progress-meter \
-  --request PUT "https://es01:9200/_index_template/spark"\
+  --request PUT "https://es01:9200/_index_template/spark-logs"\
   --cacert config/certs/ca/ca.crt \
   -u "elastic:${ELASTIC_PASSWORD}" \
-  -H "Content-Type: application/json" \
-  -d "@init/Accumulables-mapping.json"
-result = $?
+  -H "Content-Type: application/json"\
+  -d '@init/sparkIndexTemplate.json'
+result=$?
 echo -e "\nResult is $result"
 if [ $result -ne 0 ]; then
     echo "Error: curl failed with code $?" >&2
@@ -25,13 +28,10 @@ curl --no-progress-meter \
   --request PUT "https://es01:9200/_data_stream/logs-spark-spark"\
   --cacert config/certs/ca/ca.crt \
   -u "elastic:${ELASTIC_PASSWORD}" \
-  -H "Content-Type: application/json" 
-result = $?
+  -H "Content-Type: application/json"
+result=$?
 echo -e "\nResult is $result"
 if [ $result -ne 0 ]; then
     echo "Error: curl failed with code $?" >&2
     exit $result
   fi
-Comment
-
-echo -n No initialization for now
