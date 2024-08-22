ee5703c4ce0d2124e67412b17f5f42bef264e872
Gary Brooks <gxbrooks@gmail.com>
2024-08-22 13:14:13 -0500

    Migrated to Elastic Agent and fully integrated with Spark

--- init/bin/init-index.sh
+++ init/bin/init-index.sh
@@ -13,23 +13,19 @@ until curl --no-progress-meter --cacert config/certs/ca/ca.crt https://es01:9200
 # The sparkUnproccessedIndexTemplate.json file assumes the namespace of the index and the dataset are both
 # named 'spark'. These names are specified in the logstash.conf file. 
 
-init/bin/rapi.sh PUT /_ilm/policy/batch-active init/batch-active/batch-active.ilm.json
-init/bin/rapi.sh PUT /_index_template/batch-active-index init/batch-active/batch-active-index.template.json
-init/bin/rapi.sh PUT /batch-active-index-000001 init/batch-active/batch-active-index.index.json
+rapi PUT /_ilm/policy/batch-active init/batch-active/batch-active.ilm.json
+rapi PUT /_index_template/batch-active-index init/batch-active/batch-active-index.template.json
+rapi PUT /batch-active-index-000001 init/batch-active/batch-active-index.index.json
 
 
-init/bin/rapi.sh PUT /_ilm/policy/spark-logs init/spark/spark-logs.ilm.json
-init/bin/rapi.sh PUT /_index_template/logs-spark-spark init/spark/logs-spark-spark.template.json
+rapi PUT /_ilm/policy/spark-logs init/spark/spark-logs.ilm.json
+rapi PUT /_index_template/logs-spark-spark init/spark/logs-spark-spark.template.json
 
-init/bin/rapi.sh PUT /_ilm/policy/data-pipeline init/data-pipeline/data-pipeline.ilm.json
-init/bin/rapi.sh PUT /_index_template/data-pipeline-ds init/data-pipeline/data-pipeline-ds.template.json
+rapi PUT /_ilm/policy/data-pipeline init/data-pipeline/data-pipeline.ilm.json
+rapi PUT /_index_template/data-pipeline-ds init/data-pipeline/data-pipeline-ds.template.json
 
 # Need full license to run watchers
-init/bin/rapi.sh POST /_license/start_trial?acknowledge=true
+rapi POST /_license/start_trial?acknowledge=true
 
-init/bin/rapi.sh PUT /_watcher/watch/batch-match init/batch-active/match.watcher.json 
-init/bin/rapi.sh PUT /_watcher/watch/delete-matched init/batch-active/delete-matched.watcher.json
-
-# init/bin/rapi.sh PUT /_ingest/pipeline/spark-pipeline init/spark-pipeline.json
-
-# init/bin/rapi.sh PUT /_watcher/watch/spark_batch_watcher init/spark.match.watcher.json
+rapi PUT /_watcher/watch/batch-match init/batch-active/match.watcher.json 
+rapi PUT /_watcher/watch/delete-matched init/batch-active/delete-matched.watcher.json
---
+++ spark/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SPARK_WORKLOAD=$1
+
+echo "Starting Elastic Agent in background"
+
+/usr/bin/elastic-agent run &
+
+echo "SPARK_WORKLOAD: $SPARK_WORKLOAD"
+
+if [ "$SPARK_WORKLOAD" == "master" ];
+then
+  start-master.sh -p 7077
+elif [ "$SPARK_WORKLOAD" == "worker" ];
+then
+  start-worker.sh spark://spark-master:7077
+elif [ "$SPARK_WORKLOAD" == "history" ]
+then
+  start-history-server.sh
+fi
